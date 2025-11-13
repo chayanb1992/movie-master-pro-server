94cf47701ae8d1812b39df723c38ab8d46e60167
Chayan Bala <chayankumarb1992@gmail.com>
2025-11-13 10:13:36 +0600

    Update movies data in Mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 const admin = require("firebase-admin");
@@ -73,10 +73,104 @@ async function run() {
         res.status(500).send({ message: "Failed to fetch top-rated movies" });
       }
     });
+    app.get("/recently-added", async (req, res) => {
+      try {
+        const recentMovies = await movieCollection
+          .find({}, { projection: { _id: 0 } })
+          .sort({ createdAt: -1 })
+          .limit(6)
+          .toArray();
+        res.send(recentMovies);
+      } catch (error) {
+        console.error("Error fetching recently added movies:", error);
+        res
+          .status(500)
+          .send({ message: "Failed to fetch recently added movies" });
+      }
+    });
     app.post("/users", async (req, res) => {
       const data = req.body;
       const result = await movieCollection.insertOne(data);
       res.send(result);
+    });
+
+    app.post("/movies/add", async (req, res) => {
+      const movie = req.body;
+      console.log(movie);
+      const result = await movieCollection.insertOne(movie);
+      res.send(result);
+    });
+    // Get all movies added by a specific user
+    app.get("/my-collection", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) return res.status(400).json({ message: "Email required" });
+
+      try {
+        const result = await movieCollection.find({ addedBy: email }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Failed to fetch movies" });
+      }
+    });
+    app.get("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      try {
+        const movie = await movieCollection.findOne(query);
+        // console.log(movie);
+        if (!movie) {
+          return res.status(404).json({ message: "Movie not found" });
+        }
+        res.json(movie);
+      } catch (err) {
+        res.status(500).json({ message: "Error fetching movie" });
+      }
+    });
+
+    // Delete a movie by ID
+    app.delete("/delete-movie/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      try {
+        const result = await movieCollection.deleteOne(query);
+        if (result.deletedCount === 1) {
+          res.json({ success: true });
+        } else {
+          res.status(404).json({ message: "Movie not found" });
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Failed to delete movie" });
+      }
+    });
+
+    app.patch("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      console.log(updatedData.title);
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          title: updatedData.title,
+          genre: updatedData.genre,
+          releaseYear: updatedData.releaseYear,
+          director: updatedData.director,
+          cast: updatedData.cast,
+          rating: updatedData.rating,
+          duration: updatedData.duration,
+          plotSummary: updatedData.plotSummary,
+          posterUrl: updatedData.posterUrl,
+          language: updatedData.language,
+          country: updatedData.country,
+        },
+      };
+      try {
+        const result = await movieCollection.updateOne(query, update);
+        res.json(result);
+      } catch (err) {
+        res.status(500).json({ message: "Error updating movie" });
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
